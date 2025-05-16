02433b6462c2e3d66382cfc5c918809f6fbc64ba
Nichanun <nichanun.luck@gmail.com>
2025-05-15 23:11:52 -0700

    add ga

--- src/data/certificateData.js
+++ src/data/certificateData.js
@@ -1,4 +1,5 @@
 export const certificates = [
+  { name: "Google Advanced Data Analytics Specialization", year: "Aug 2024" },
   { name: "AppsFlyer Masterclass Certified", year: "Aug 2023" },
   { name: "Google UX Design", year: "Mar 2023" },
   { name: "AWS Certified Developer – Associate", year: "Dec 2017" },
