87a4bca526f2349bac9e04cb2e59b1ea1ba42701
Nichanun <nichanun.luck@gmail.com>
2025-05-19 05:06:28 -0700

    add projects

--- src/data/workCardData.jsx
+++ src/data/workCardData.jsx
@@ -1,12 +1,13 @@
 import pro1 from "../assets/project1.png";
 import pro2 from "../assets/project2.png";
 import pro3 from "../assets/project3.png";
+import pro4 from "../assets/project4.png";
 const ProjectCardData = [
   {
     imgsrc: pro1,
     title: "Building Permits",
     text: "An interactive dashboard visualizing building permits issued by the City of Vancouver. Explore annual permit trends, and project values through dynamic charts and filters.",
-    techno: ["D3.js", "CSS", "JavaScript", "HTML"],
+    techno: ["D3.js", "JavaScript", "HTML", "CSS"],
     view: "https://nichanunpong.github.io/buildingpermits/",
     source: "https://github.com/nichanunpong/buildingpermits",
   },
@@ -14,7 +15,7 @@ const ProjectCardData = [
     imgsrc: pro2,
     title: "Food App",
     text: "A recipe search app that lets you find dishes by ingredient, view step-by-step instructions. Built using the Spoonacular API for live recipe data.",
-    techno: ["react", "CSS"],
+    techno: ["React", "HTML", "CSS"],
     view: "https://nichanunpong.github.io/foodapp/",
     source: "https://github.com/nichanunpong/foodapp",
   },
@@ -25,6 +26,14 @@ const ProjectCardData = [
     techno: ["React", "Vite", "HTML", "CSS"],
     view: "https://nichanunpong.github.io/todoapp/",
     source: "https://github.com/nichanunpong/todoapp",
+  },
+  {
+    imgsrc: pro4,
+    title: "Supper Store Dashboard",
+    text: "The Support Store dashboard visualizes the annual sales report, helping the owner understand their customers and increase sales.",
+    techno: ["Tableau"],
+    view: "https://public.tableau.com/views/Assign1_Nichanun_Pon_300402500/Story?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link",
+    source: "https://github.com/nichanunpong/tableau_supperstore",
   },
 ];
 
